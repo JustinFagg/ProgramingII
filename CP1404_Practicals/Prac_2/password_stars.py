20abf611aac30fb364b7a4b47a27f188b58c19bb
JustinFagg <justin.fagg@my.jcu.edu.au>
2022-09-23 22:04:35 +1000

    Changed password checker to run with functions

--- CP1404_Practicals/Prac_2/password_stars.py
+++ CP1404_Practicals/Prac_2/password_stars.py
@@ -1,6 +1,19 @@
-minimum_password_length = int(input("Password Length: "))
-user_password = input("Password: ")
-while minimum_password_length > len(user_password):
-    print("Password needs to be longer")
+def main(minimum_password_length):
+    user_password = method_name(minimum_password_length)
+    print_hidden_password(user_password)
+    return user_password
+
+
+def print_hidden_password(user_password):
+    print("*" * len(user_password))
+
+
+def method_name(minimum_password_length):
     user_password = input("Password: ")
-print("*" * len(user_password))
+    while minimum_password_length > len(user_password):
+        user_password = input("Password: ")
+    return user_password
+
+
+main(5)
+
