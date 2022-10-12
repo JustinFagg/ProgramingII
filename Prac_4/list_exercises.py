c9c1cc65708685fd52e245d853e15aff44b8353f
JustinFagg <justin.fagg@my.jcu.edu.au>
2022-10-12 19:54:08 +1000

    added Woefully inadequate security checker

--- Prac_4/list_exercises.py
+++ Prac_4/list_exercises.py
@@ -14,5 +14,9 @@ def main():
     print(f"The largest number is {max(numbers)}")
     print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+    if input("Username: ") in usernames:
+        print("Access granted")
+
 
 main()
