79b7ba329e4523a455e68876eda37c8e817ed31c
JustinFagg <justin.fagg@my.jcu.edu.au>
2022-10-17 17:20:58 +1000

    Complete prac 5 word occurrences exercise

---
+++ Prac_5/word_occurrences.py
@@ -0,0 +1,17 @@
+"""
+Word Occurrences
+Estimate: 20 minutes
+Actual:   16 minutes
+"""
+
+word_counts = {}
+
+text = input("Text: ").lower()
+text_words = text.split()
+for word in text_words:
+    try:
+        word_counts[word] = word_counts[word] + 1
+    except KeyError:
+        word_counts[word] = 1
+for word in sorted(word_counts):
+    print(f"{word:{13}} : {word_counts[word]}")
