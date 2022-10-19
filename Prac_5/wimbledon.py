e68a8d7f037e86d9dfc467cefb40e9333cb1a185
JustinFagg <justin.fagg@my.jcu.edu.au>
2022-10-19 22:04:50 +1000

    Finished wimbledon.py

--- Prac_5/wimbledon.py
+++ Prac_5/wimbledon.py
@@ -1,25 +1,46 @@
+"""
+Word Occurrences
+Estimate: 40 minutes
+Actual:   51 minutes
+"""
+
+
 FILE_NAME = "test.csv"
 
-win_counts = {}
-champion_countries = set()
 
-with open(FILE_NAME, "r", encoding="utf-8-sig") as in_file:
-    file_data = []
-    next(in_file)
-    for line in in_file:
-        file_data.append(line.split(","))
+def main():
+    file_data = get_data()
+    champion_countries, win_counts = scalp_data(file_data)
+    print_data(champion_countries, win_counts)
 
 
-for data in file_data:
-    champion_countries.add(data[1])
-    try:
-        win_counts[data[2]] = win_counts[data[2]] + 1
-    except KeyError:
-        win_counts[data[2]] = 1
+def print_data(champion_countries, win_counts):
+    print("Wimbledon Champions:")
+    for champion in win_counts:
+        print(f"{champion} {win_counts[champion]}")
+    print(f"\nThese {len(champion_countries)} countries have won Wimbledon: ")
+    print(f", ".join(sorted(champion_countries)))
 
-print("Wimbledon Champions:")
-for champion in win_counts:
-    print(f"{champion} {win_counts[champion]}")
-print(f"\nThese {len(champion_countries)} countries have won Wimbledon: ")
-print(f", ".join(sorted(champion_countries)))
 
+def scalp_data(file_data):
+    win_counts = {}
+    champion_countries = set()
+    for data in file_data:
+        champion_countries.add(data[1])
+        try:
+            win_counts[data[2]] = win_counts[data[2]] + 1
+        except KeyError:
+            win_counts[data[2]] = 1
+    return champion_countries, win_counts
+
+
+def get_data():
+    with open(FILE_NAME, "r", encoding="utf-8-sig") as in_file:
+        file_data = []
+        next(in_file)
+        for line in in_file:
+            file_data.append(line.split(","))
+    return file_data
+
+
+main()
