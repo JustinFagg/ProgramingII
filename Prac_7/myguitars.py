c981cddd64f1a471f30599990cac116cdcfad3ec
JustinFagg <justin.fagg@my.jcu.edu.au>
2022-11-09 12:27:53 +1000

    Complete last part of More Guitars exercise

--- Prac_7/myguitars.py
+++ Prac_7/myguitars.py
@@ -10,10 +10,10 @@ FILE = 'guitars.csv'
 def main():
     my_guitars = []
     opened_file = open(FILE, 'r')
-    opened_file.readline()
     for line in opened_file:
         parts = line.strip().split(',')
         my_guitars.append(Guitar(parts[0], int(parts[1]), float(parts[2])))
+    opened_file.close()
 
     my_guitars.sort()
 
@@ -21,6 +21,16 @@ def main():
         is_guitar_vintage_string = "(vintage)" if my_guitars[i].is_vintage() else ""
         print(f"Guitar {i + 1}: {my_guitars[i].name:>25} ({my_guitars[i].year:>4}),"
               f" worth $ {my_guitars[i].cost:10,.2f} {is_guitar_vintage_string}")
+
+    guitar_name = input("Name: ")
+    while guitar_name != "":
+        my_guitars.append(Guitar(guitar_name, int(input("Year: ")), float(input("Cost: "))))
+        guitar_name = input("Name: ")
+
+    opened_file = open(FILE, 'w')
+    for i in range(len(my_guitars)):
+        print(f"{my_guitars[i].name},{my_guitars[i].year},{my_guitars[i].cost}", file=opened_file)
+    opened_file.close()
 
 
 main()
